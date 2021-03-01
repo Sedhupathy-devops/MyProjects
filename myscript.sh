b0db27e881bfafe062ef9b07b3df12e9bf31339c
Sedhupathy-devops <78788595+Sedhupathy-devops@users.noreply.github.com>
2021-03-02 01:58:43 +0530

    Update myscript.sh
    Description has been added for better understanding.

--- myscript.sh
+++ myscript.sh
@@ -5,12 +5,13 @@ if [ -e ~/filelist.txt ]
 then
 rm ~/filelist.txt
 fi
+#File existing detial checked and if file exists it will be deleted to create new file to store detials.
 echo >>~/filelist.txt
 echo "----------------This file contains the file-detials of $(whoami)----------------">>~/filelist.txt
 echo >>~/filelist.txt
 echo "The Directories in $(whoami) are" >>~/filelist.txt
 echo >>~/filelist.txt
-
+#To search the directories and list out
 for i in $(ls -l ~ | grep ^d | awk '{print$9}')
 do
 ((a++))
@@ -23,7 +24,7 @@ echo >>~/filelist.txt
 
 echo>>~/filelist.txt
 echo "The Files in $(whoami) are">>~/filelist.txt
-
+#To search the files and list out
 a=0
 for i in $(ls -l ~ | grep ^- | awk '{print$9}')
 do
@@ -33,3 +34,4 @@ echo "        >>>>>file_$a--->$i">>~/filelist.txt
 done
 echo >>~/filelist.txt
 echo "Total No. of files : $(ls -l ~ | grep ^- | awk '{print$9}'| wc -l )">>~/filelist.txt
+#THE END
